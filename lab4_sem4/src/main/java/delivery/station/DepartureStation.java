a0f70d19df97943236df7de4f31454d995ad740f
Nikita Kosarev <kosarev4392@gmail.com>
2021-04-05 13:07:54 +0700

    Version 1.1
    Added executor service

--- lab4_sem4/src/main/java/delivery/station/DepartureStation.java
+++ lab4_sem4/src/main/java/delivery/station/DepartureStation.java
@@ -5,27 +5,62 @@ import main.ConfigFormatException;
 import main.Infrastructure;
 import production.ProductType;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class DepartureStation extends Station implements TrainCreator {
+    private class CreationRunnable implements Runnable {
+        private final ProductType productType;
+        private final int toDeliver;
+        private final int timeToCreate;
+
+        private CreationRunnable(ProductType productType, int toDeliver, int timeToCreate) {
+            this.productType = productType;
+            this.toDeliver = toDeliver;
+            this.timeToCreate = timeToCreate;
+        }
+
+        @Override
+        public void run() {
+            final long SECOND_MILLIS = 1000;
+            int amortisation = Integer.parseInt(Infrastructure.getProperties().get("train_amortisation").toString());
+
+            try {
+                Thread.sleep(this.getTimeToCreate() * SECOND_MILLIS);
+                Train newTrain = new Train(productType, amortisation, toDeliver);
+                putTrainInto(newTrain);
+                new Thread(newTrain).start();
+            } catch (InterruptedException ignored) {
+            }
+        }
+
+        public ProductType getProductType() {
+            return productType;
+        }
+
+        public int getToDeliver() {
+            return toDeliver;
+        }
+
+        public int getTimeToCreate() {
+            return timeToCreate;
+        }
+    }
+
     private final int timeToCreate;
+    private final ExecutorService trainCreationPool;
+
     public DepartureStation() throws ConfigFormatException {
         super();
 
         this.timeToCreate = Integer.parseInt(Infrastructure.getProperties().get("depot_timeToCreate").toString());
+        this.trainCreationPool = Executors.newCachedThreadPool();
     }
 
     @Override
     public void createTrain(ProductType productType, int toDeliver) {
-        final long SECOND_MILLIS = 1000;
-        int amortisation = Integer.parseInt(Infrastructure.getProperties().get("train_amortisation").toString());
-
-        try {
-            Thread.sleep(this.getTimeToCreate() * SECOND_MILLIS);
-            Train newTrain = new Train(productType, amortisation, toDeliver);
-            this.putTrainInto(newTrain);
-            new Thread(newTrain).start();
-        } catch (InterruptedException e) {
-            return;
-        }
+        CreationRunnable create = new CreationRunnable(productType, toDeliver, timeToCreate);
+        this.trainCreationPool.submit(create);
     }
 
     public int getTimeToCreate() {
